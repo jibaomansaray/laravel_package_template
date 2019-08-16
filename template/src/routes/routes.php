9d3a85ac12fe6eb455854b98505be2f00692b4a3
Jibao Mansaray <jibaomansaray@gmail.com>
2019-08-17 00:07:41 +1000

    Add a route to handle all request to the package

--- template/src/routes/routes.php
+++ template/src/routes/routes.php
@@ -3,3 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('vendornamepackagename','Vendorname\Packagename\Controllers\HelloController@indexAction');
+
+// Add your routes before the following route
+
+
+if (config('env') != 'production') {
+    /**
+     * Make sure to publish your public assets when going live
+     */
+    Route::get('vendornamepackagename/{path?}', 'Vendorname\Packagename\Controllers\AssetController@serveAction')->where('path', '(.*)');
+}
