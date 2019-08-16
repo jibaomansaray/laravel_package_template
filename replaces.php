787ff2d1590efa2360c5752c85a7a65167e62b3a
Jibao Mansaray <jibaomansaray@gmail.com>
2019-08-16 12:42:48 +1000

    Add file with words to replace

---
+++ replaces.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+      'Vendorname' => ucfirst($vendor),
+      'Packagename' => ucfirst($package),
+      'vendorname' => lcfirst($vendor),
+      'packagename' => lcfirst($package),
+];
